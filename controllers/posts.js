42b3d2098c53dc1b3726dccac61fd50deb86f298
Hoang Nguyen Xuan <xhoang0509@gmail.com>
2022-07-27 09:47:51 +0700

    update code

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,6 @@ import { PostModel } from "../models/PostModel.js";
 export const getPosts = async (req, res) => {
   try {
     const posts = await PostModel.find();
-    console.log("posts", posts);
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ error });
